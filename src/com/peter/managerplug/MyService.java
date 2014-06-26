ebedcdd6e6ff2a59898d3e2df45b881135b99ab6
zhangrui <zhangrui@maxthon.net>
2014-06-26 12:44:23 +0800

    加入自动关闭功能

--- src/com/peter/managerplug/MyService.java
+++ src/com/peter/managerplug/MyService.java
@@ -7,9 +7,6 @@ import android.widget.Toast;
 
 public class MyService extends Service {
 
-	public static final String TARGET_PACKAGE_NAME = "com.peter.appmanager";
-	public static final String TARGET_ACTION = "com.peter.appmanager";
-	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -18,19 +15,17 @@ public class MyService extends Service {
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Toast.makeText(this, "Plug Service onStartCommand()", Toast.LENGTH_SHORT).show();
-		return START_STICKY;
+		return START_NOT_STICKY;
 	}
 
 	@Override
 	public void onCreate() {
 		Toast.makeText(this, "Plug Service onCreate()", Toast.LENGTH_SHORT).show();
-		startService(new Intent(TARGET_ACTION));
+		stopSelf();
 	}
 
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, "Plug Service onDestroy()", Toast.LENGTH_SHORT).show();
-		Intent intent = new Intent(getApplicationContext(), MyService.class);
-		startService(intent);
 	}
 }
--- src/com/peter/managerplug/RestartReceiver.java
+++ src/com/peter/managerplug/RestartReceiver.java
@@ -8,15 +8,18 @@ import android.widget.Toast;
 
 public class RestartReceiver extends BroadcastReceiver {
 
+	public static final String TARGET_PACKAGE_NAME = "com.peter.appmanager";
+	public static final String TARGET_ACTION = "com.peter.appmanager";
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		Uri url = intent.getData();
 		String str = url.toString();
 		String[] infos = str.split(":");
 		for(String info : infos) {
-			if(info.equals(MyService.TARGET_PACKAGE_NAME)) {
+			if(info.equals(TARGET_PACKAGE_NAME)) {
 				Toast.makeText(context, "YinService RestartReceiver", Toast.LENGTH_SHORT).show();
-				context.startService(new Intent(MyService.TARGET_ACTION));
+				context.startService(new Intent(TARGET_ACTION));
 			}
 		}
 	}
